f4e1f261c31e00619b524af6a9d4689b22682b5c
Fahad Ashraf <bcsf14a535@pucit.edu.pk>
2018-12-19 13:55:13 +0500

    Footer changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  color = 'red';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getColor() {
+    return this.color;
   }
 
 }
